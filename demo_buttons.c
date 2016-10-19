fd403bdaf95ffa0d3708ebdad623f447bc4054ab
Matthias <mail@matthiasbock.net>
2016-10-19 22:15:45 +0200

    reading a button works

--- demo_buttons.c
+++ demo_buttons.c
@@ -12,9 +12,17 @@ void main()
 
     while (1)
     {
-        nrf_gpio_pin_set(PIN_LED1);
-        nrf_delay_us(500000);
-        nrf_gpio_pin_clear(PIN_LED1);
-        nrf_delay_us(500000);
+        // button pressed = 0
+        if (!nrf_gpio_pin_read(PIN_BUTTON1))
+        {
+            // LED on
+            nrf_gpio_pin_clear(PIN_LED1);
+        }
+        else
+        {
+            // LED off
+            nrf_gpio_pin_set(PIN_LED1);
+        }
+        nrf_delay_us(10000);
     }
 }
